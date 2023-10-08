3eb09f5c34c8bcf9e93b92fa98233e370f67d2f9
Poyraz INAN <ben@poyrazinan.com.tr>
2023-10-08 05:56:45 +0300

    created user object
    + created user object

---
+++ src/main/java/net/leaderos/plugin/model/User.java
@@ -0,0 +1,74 @@
+package net.leaderos.plugin.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * User object
+ *
+ * @author poyrazinan
+ * @since 1.0
+ */
+@AllArgsConstructor
+public class User {
+
+    /**
+     * Player id on website
+     */
+    @Getter
+    private String id;
+
+    /**
+     * Credit amount of player as double
+     */
+    @Getter
+    @Setter
+    private double credit;
+
+    /**
+     * Username of player on website
+     */
+    @Getter
+    @Setter
+    private String username;
+
+    /**
+     * Email of player on website
+     */
+    @Getter
+    @Setter
+    private String email;
+
+    /**
+     * Creation ip of player on website
+     */
+    @Getter
+    private String creationIp;
+
+    /**
+     * Creation date of player on website
+     */
+    @Getter
+    private Date creationDate;
+
+    /**
+     * Constructor of User object
+     * @param user JSONObject of user
+     */
+    public User(@NotNull JSONObject user) {
+        this.id = user.getString("id");
+        this.credit = Double.parseDouble(user.getString("credit"));
+        this.username = user.getString("username");
+        this.email = user.getString("email");
+        this.creationIp = user.getString("creationIp");
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        this.id = format.format(user.getString("creationDate"));
+    }
+}
