8c689f9c1bf7312395e4ccbb12d963b96f13c45c
Fırat Kaya <48600092+benfiratkaya@users.noreply.github.com>
2024-08-15 12:53:35 +0300

    1.1.5: version bump

--- velocity/src/main/java/net/leaderos/velocity/Velocity.java
+++ velocity/src/main/java/net/leaderos/velocity/Velocity.java
@@ -45,7 +45,7 @@ import java.nio.file.Path;
 @Plugin(
         id = "leaderos",
         name = "LeaderOS",
-        version = "1.1.4",
+        version = "1.1.5",
         url = "https://leaderos.net",
         description = "LeaderOS Plugin for Velocity",
         authors = {"leaderos"}
@@ -209,7 +209,7 @@ public class Velocity {
 
     public void checkUpdate() {
         Velocity.getInstance().getServer().getScheduler().buildTask(Velocity.getInstance(), () -> {
-            PluginUpdater updater = new PluginUpdater("1.1.4");
+            PluginUpdater updater = new PluginUpdater("1.1.5");
             try {
                 if (updater.checkForUpdates()) {
                     Component msg = ChatUtil.replacePlaceholders(
