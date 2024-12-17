c0c4b2a4df82e146df6c74087ff64abc0f43232e
Fırat Kaya <48600092+benfiratkaya@users.noreply.github.com>
2024-12-17 11:21:23 +0300

    1.1.7: dep updated

--- velocity/src/main/java/net/leaderos/velocity/Velocity.java
+++ velocity/src/main/java/net/leaderos/velocity/Velocity.java
@@ -45,7 +45,7 @@ import java.nio.file.Path;
 @Plugin(
         id = "leaderos",
         name = "LeaderOS",
-        version = "1.1.6",
+        version = "1.1.7",
         url = "https://leaderos.net",
         description = "LeaderOS Plugin for Velocity",
         authors = {"leaderos"}
@@ -209,7 +209,7 @@ public class Velocity {
 
     public void checkUpdate() {
         Velocity.getInstance().getServer().getScheduler().buildTask(Velocity.getInstance(), () -> {
-            PluginUpdater updater = new PluginUpdater("1.1.6");
+            PluginUpdater updater = new PluginUpdater("1.1.7");
             try {
                 if (updater.checkForUpdates()) {
                     Component msg = ChatUtil.replacePlaceholders(
